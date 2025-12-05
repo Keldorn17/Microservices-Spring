f63a93fc51d1b8e434fb11cd048d2bd25e353f0a
Patai Zoltán <patai17.zoltan17@gmail.com>
2025-12-05 14:04:08 +0100

    Using RestClient

--- OrderService/src/main/java/com/keldorn/orderservice/client/InventoryClient.java
+++ OrderService/src/main/java/com/keldorn/orderservice/client/InventoryClient.java
@@ -1,14 +1,11 @@
 package com.keldorn.orderservice.client;
 
 import com.keldorn.orderservice.dto.BooleanResponse;
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.service.annotation.GetExchange;
 
-@FeignClient(value = "inventory", url = "${inventory.url}")
 public interface InventoryClient {
 
-    @RequestMapping(method = RequestMethod.GET, value = "/api/inventory")
+    @GetExchange("/api/inventory")
     BooleanResponse isInStock(@RequestParam String skuCode, @RequestParam Integer quantity);
 }
