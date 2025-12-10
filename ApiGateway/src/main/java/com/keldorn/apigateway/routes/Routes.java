dc85945f22176b8699983d55a2a810bb41102dc4
Patai Zoltán <patai17.zoltan17@gmail.com>
2025-12-10 18:26:07 +0100

    Resilience4j

--- ApiGateway/src/main/java/com/keldorn/apigateway/routes/Routes.java
+++ ApiGateway/src/main/java/com/keldorn/apigateway/routes/Routes.java
@@ -1,11 +1,15 @@
 package com.keldorn.apigateway.routes;
 
 import lombok.NonNull;
+import org.springframework.cloud.gateway.server.mvc.filter.CircuitBreakerFilterFunctions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerResponse;
+
+import java.net.URI;
 
 import static org.springframework.cloud.gateway.server.mvc.filter.BeforeFilterFunctions.setPath;
 import static org.springframework.cloud.gateway.server.mvc.filter.BeforeFilterFunctions.uri;
@@ -21,6 +25,8 @@ public class Routes {
         return route("product_service")
                 .GET("/api/product", http())
                 .POST("/api/product", accept(MediaType.APPLICATION_JSON), http())
+                .filter(CircuitBreakerFilterFunctions.circuitBreaker("productServiceCircuitBreaker",
+                        URI.create("forward:/fallbackRoute")))
                 .before(uri("http://localhost:8080"))
                 .build();
     }
@@ -29,6 +35,8 @@ public class Routes {
     public RouterFunction<@NonNull ServerResponse> productServiceSwaggerRoute() {
         return route("product_service_swagger")
                 .GET("/aggregate/product-service/v3/api-docs", http())
+                .filter(CircuitBreakerFilterFunctions.circuitBreaker("productServiceSwaggerCircuitBreaker",
+                        URI.create("forward:/fallbackRoute")))
                 .before(uri("http://localhost:8080"))
                 .before(setPath("/api-docs"))
                 .build();
@@ -38,6 +46,8 @@ public class Routes {
     public RouterFunction<@NonNull ServerResponse> orderServiceRoute() {
         return route("order_service")
                 .POST("/api/order", accept(MediaType.APPLICATION_JSON), http())
+                .filter(CircuitBreakerFilterFunctions.circuitBreaker("orderServiceCircuitBreaker",
+                        URI.create("forward:/fallbackRoute")))
                 .before(uri("http://localhost:8081"))
                 .build();
     }
@@ -46,6 +56,8 @@ public class Routes {
     public RouterFunction<@NonNull ServerResponse> orderServiceSwaggerRoute() {
         return route("order_service_swagger")
                 .GET("/aggregate/order-service/v3/api-docs", http())
+                .filter(CircuitBreakerFilterFunctions.circuitBreaker("orderServiceSwaggerCircuitBreaker",
+                        URI.create("forward:/fallbackRoute")))
                 .before(uri("http://localhost:8081"))
                 .before(setPath("/api-docs"))
                 .build();
@@ -55,6 +67,8 @@ public class Routes {
     public RouterFunction<@NonNull ServerResponse> inventoryServiceRoute() {
         return route("inventory_service")
                 .route(r -> r.path().startsWith("/api/inventory"), http())
+                .filter(CircuitBreakerFilterFunctions.circuitBreaker("inventoryServiceCircuitBreaker",
+                        URI.create("forward:/fallbackRoute")))
                 .before(uri("http://localhost:8082"))
                 .build();
     }
@@ -63,8 +77,19 @@ public class Routes {
     public RouterFunction<@NonNull ServerResponse> inventoryServiceSwaggerRoute() {
         return route("inventory_service_swagger")
                 .GET("/aggregate/inventory-service/v3/api-docs", http())
+                .filter(CircuitBreakerFilterFunctions.circuitBreaker("inventoryServiceSwaggerCircuitBreaker",
+                        URI.create("forward:/fallbackRoute")))
                 .before(uri("http://localhost:8082"))
                 .before(setPath("/api-docs"))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<@NonNull ServerResponse> fallbackRoute() {
+        return route("fallbackRoute")
+                .GET("/fallbackRoute", request ->
+                        ServerResponse.status(HttpStatus.SERVICE_UNAVAILABLE)
+                                .body("Service Unavailable, please try again later."))
                 .build();
     }
 }
--- OrderService/src/main/java/com/keldorn/orderservice/config/RestClientConfig.java
+++ OrderService/src/main/java/com/keldorn/orderservice/config/RestClientConfig.java
@@ -4,9 +4,14 @@ import com.keldorn.orderservice.client.InventoryClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.JdkClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+import java.net.http.HttpClient;
+import java.time.Duration;
 
 @Configuration
 public class RestClientConfig {
@@ -18,9 +23,20 @@ public class RestClientConfig {
     public InventoryClient inventoryClient() {
         RestClient restClient = RestClient.builder()
                 .baseUrl(inventoryServiceUrl)
+                .requestFactory(getClientRequestFactory())
                 .build();
         var restClientAdapter = RestClientAdapter.create(restClient);
         var httpServiceProxyFactory = HttpServiceProxyFactory.builderFor(restClientAdapter).build();
         return httpServiceProxyFactory.createClient(InventoryClient.class);
+    }
+
+    private ClientHttpRequestFactory getClientRequestFactory() {
+        HttpClient httpClient = HttpClient.newBuilder()
+                .connectTimeout(Duration.ofSeconds(3))
+                .version(HttpClient.Version.HTTP_2)
+                .build();
+        JdkClientHttpRequestFactory requestFactory = new JdkClientHttpRequestFactory(httpClient);
+        requestFactory.setReadTimeout(Duration.ofSeconds(3));
+        return requestFactory;
     }
 }
